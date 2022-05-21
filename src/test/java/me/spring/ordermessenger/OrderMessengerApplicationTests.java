4d46fa125f37983fe65e8198be2273d85d352415
wanniDev <wannidev0928@gmail.com>
2022-05-22 00:36:12 +0900

    init project

---
+++ src/test/java/me/spring/ordermessenger/OrderMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.spring.ordermessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
