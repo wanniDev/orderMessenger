00c55c25b4a02bfc71198cf2abcf0cda93885604
wanniDev <wannidev0928@gmail.com>
2022-05-22 17:48:10 +0900

    Feat: 주문 생성 api 구현

--- producer/src/main/java/me/spring/producer/service/OrderService.java
+++ producer/src/main/java/me/spring/producer/service/OrderService.java
@@ -1,8 +1,15 @@
 package me.spring.producer.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.RequiredArgsConstructor;
+import me.spring.model.BrokerMessageLog;
+import me.spring.model.Order;
 import me.spring.producer.repository.BrokerMessageLogRepository;
 import me.spring.producer.repository.OrderRepository;
 import me.spring.producer.sender.RabbitOrderSender;
@@ -15,4 +22,23 @@ public class OrderService {
 	private final BrokerMessageLogRepository brokerMessageLogRepository;
 
 	private final RabbitOrderSender orderSender;
+
+	private final ObjectMapper objectMapper;
+
+	public void createOrder(Order order) {
+		try {
+			LocalDateTime now = LocalDateTime.now();
+			orderRepository.save(order);
+			String orderMessage = objectMapper.writeValueAsString(order);
+			BrokerMessageLog brokerMessageLog = BrokerMessageLog.builder()
+				.message(orderMessage)
+				.createTime(now)
+				.updateTime(now)
+				.build();
+			brokerMessageLogRepository.save(brokerMessageLog);
+			orderSender.send(order);
+		} catch (JsonProcessingException e) {
+			throw new IllegalArgumentException(e.getMessage());
+		}
+	}
 }
