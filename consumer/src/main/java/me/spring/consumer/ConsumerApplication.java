6798118374ee5f86895c4781b044dd0e28d715a5
wanniDev <wannidev0928@gmail.com>
2022-05-22 21:36:14 +0900

    Fix: model 모듈의 엔티티를 스캔하는 애노테이션 추가

--- consumer/src/main/java/me/spring/consumer/ConsumerApplication.java
+++ consumer/src/main/java/me/spring/consumer/ConsumerApplication.java
@@ -2,8 +2,10 @@ package me.spring.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "me.spring.model")
 public class ConsumerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
